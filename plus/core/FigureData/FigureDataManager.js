6b40fa0b7f69cbdf33c1914a34ba2651ced27db3
shunz19 <shunzyrole@gmail.com>
2020-07-12 19:35:00 +0800

    more FigureDataManager

--- plus/core/FigureData/FigureDataManager.js
+++ plus/core/FigureData/FigureDataManager.js
@@ -2,9 +2,10 @@ var xmlreader = require("xml-reader").create();
 var fs = require("fs");
 var Palette = require("./Types/Palette.js");
 var Color = require("./Types/Color.js")
+var Part = require("./Types/Part.js")
 var FigureSet = require("./Types/FigureSet.js")
 var SetClass = require("./Types/SetClass.js")
-var SetTypeUtility = require("./Types/SetTypeUtility.js")
+var SetTypeUtility = new (require("./Types/SetTypeUtility.js"))()
 
 class FigureDataManager {
 	constructor() {
@@ -45,8 +46,8 @@ class FigureDataManager {
 				}
 
 				_this._setTypes["hd"].Sets[99999] = new SetClass(99999, "U", 0, true, false, false);
-	            console.log("Loaded " + _this._palettes.length + " Color Palettes");
-	            console.log("Loaded " + _this._setTypes.length + " Set Types");
+				console.log("Loaded " + Object.keys(_this._palettes).length + " Color Palettes");
+				console.log("Loaded " + Object.keys(_this._setTypes).length + " Set Types");
 
 				res();
 			})
@@ -56,15 +57,116 @@ class FigureDataManager {
 	}
 
 	ProcessFigure(figure, gender, clothingParts, hasHabboClub) {
+		figure = figure.toLowerCase();
+		gender = gender.toUpperCase();
+		var rebuildFigure = "";
+		var figureParts = figure.split(".");
+		/* @region Check clothing, colors & Habbo Club */
+		for(var i = 0, len = figureParts.length; i < len; i++) {
+			var part = figureParts[i];
+			var partSplit = part.split("-")
+			var type = partSplit[0];
+			var figureSet = this._setTypes[type];
+			if(figureSet) {
+				var partId = parseInt(partSplit[1]);
+				var colorId = 0;
+				var secondColorId = 0;
+				var set = figureSet.Sets[partId];
+				if(set) {
+					// @region Gender Check
+					if(set.Gender != gender && set.Gender != "U") {
+						var filtered = figureSet.Sets.filter(x=>x.Gender == gender || x.Gender == "U");
+						if(filtered.length > 0) {
+							partId = filtered[0].Id;
+							set = filtered[0];
+						}
+					}
 
+					// @region Colors
+					if(set.Colorable) {
+						switch(partSplit.length - 1) {
+							case 3:
+							case 2:
+								var _tn = parseInt(partSplit[2]);
+								// @region First Color
+								if(!isNaN(_tn)) {
+									colorId = _tn;
+									var palette = this.GetPalette(colorId);
+									if(colorId != 0 && (!palette || figureSet.PalletId != palette.Id)) {
+										this.GetRandomColor(figureSet.PalletId);
+									}
+								}
+
+								// @region Second Color
+								if(partSplit.length - 1 == 3) {
+									_tn = parseInt(partSplit[3]);
+									if(!isNaN(_tn)) {
+										secondColorId = _tn;
+										var palette = this.GetPalette(secondColorId);
+										if(secondColorId != 0 && (!palette || figureSet.PalletId != palette.Id)) {
+											this.GetRandomColor(figureSet.PalletId);
+										}
+									}
+								}
+							break;
+						}
+					}
+					else {
+						var ignore = ["ca","wa"];
+						if(ignore.includes(type.toLowerCase()) && partSplit[2]) colorId = parseInt(partSplit[2]);
+					}
+
+					if(set.ClubLevel > 0 && !hasHabboClub) {
+						var filtered = figureSet.Sets.filter(x=>(x.Gender == gender || x.Gender == "U") && x.ClubLevel == 0);
+						if(filtered.length > 0) {
+							partId = filtered[0].Id;
+							set = filtered[0];
+							colorId = this.GetRandomColor(figureSet.paletteId);
+						}
+					}
+					rebuildFigure += type + "-" + partId + "-" + colorId + "." + (secondColorId == 0 ? "" : secondColorId);
+				}
+			}
+		}
+
+		/* @region check required clothing */
+		for(var requirement of this._requirements) {
+			if(requirement == "ch" && gender == "M") continue;
+			if(!rebuildFigure.indexOf(requirement)) {
+				var figureSet = this._setTypes[requirement];
+				if(figureSet) {
+					var filtered = figureSet.Sets.filter(x=>x.Gender == gender || x.Gender == "U");
+					if(filtered.length > 0) {
+						partId = filtered[0].Id;
+						colorId = this.getRandomColor(figureSet.PalletId);
+						rebuildFigure += requirement + "-" + partId + "-" + colorId + ".";
+					}
+				}
+			}
+		}
+
+		/* @region Check purchasable clothing */
+		if(clothingParts) {
+
+		}
+
+		return rebuildFigure
 	}
 
 	GetPalette(colorId) {
-
+		for(var key in this._palettes) {
+			if(this._palettes[key].Colors[colorId]) return this._palettes[key];
+		}
 	}
+
 
 	TryGetPalette(paletteId, palette) {
 
+	}
+
+
+	GetRandomColor(palletId) {
+		return this._palettes[palletId].Colors[Object.keys(this._palettes[palletId].Colors)[0]].id
 	}
 }
 
--- plus/core/FigureData/Types/SetTypeUtility.js
+++ plus/core/FigureData/Types/SetTypeUtility.js
@@ -1,5 +1,7 @@
 class SetTypeUtility {
     
+    constructor() {}
+
 	GetSetType(type) {
 		var types = ["HR","HD","CH","LG","SH","HA","HE","EA","FA","CA","WA","CC","CP"];
 		return types.indexOf(type);
