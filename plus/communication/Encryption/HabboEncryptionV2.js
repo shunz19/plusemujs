e311f69e243caf6a5daf33c3bc65b93339d59724
shunz19 <shunzyrole@gmail.com>
2020-08-05 20:03:22 +0800

    Discontinuation.

---
+++ plus/communication/Encryption/HabboEncryptionV2.js
@@ -0,0 +1,7 @@
+const crypto = require("crypto");
+
+class HabboEncryptionV2 {
+	constructor(keys) {
+		this.Rsa = keys;
+	}
+}
---
+++ plus/communication/Encryption/KeyExchange/DiffielHellman.js
@@ -0,0 +1,5 @@
+class DiffielHellman {
+	constructor(ignoreBaseKeys = false) {
+		this.PublicKey = 0;
+	}
+}
